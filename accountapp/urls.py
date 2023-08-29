92bf1d0e805d645d5cc213b031af356dbfdc1396
unknown <jyoun@yewon.com>
2023-08-29 23:30:16 +0900

    dijango 22

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -8,8 +8,10 @@ app_name = "accountapp"
 urlpatterns = [
 
     path('hello_world/', hello_world, name='hello_world'),
-    path('login/', LoginView.as_view(template_name='accoutapp/login.html'), name='login'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
     path('create/', AccountCreateView.as_view(), name='create'),
 
 ]
--- yewon/settings.py
+++ yewon/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -142,3 +144,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
