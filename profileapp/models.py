924d5a50f8621195a21e403a880e6b97453a25a3
unknown <jyoun@yewon.com>
2023-09-03 15:41:43 +0900

    django 33

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 
 class Profile(models.Model):
+    objects = None
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # 유저가 탈퇴하면 이 프로필도 사라짐
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
